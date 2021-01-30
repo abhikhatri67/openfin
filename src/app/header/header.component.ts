d7450be67596118b6f6137154977388aa38a9fd1
abhikhatri67 <abhikhatri67@gmail.com>
2021-01-30 21:16:04 +0530

    Header stylin on scroll done. Overall too completed.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const number =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    if (number > 64) {
+      this.headerBackgroundColor = 'white';
+    } else {
+      this.headerBackgroundColor = 'transparent';
+    }
   }
 
+  headerBackgroundColor = 'transparent';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
